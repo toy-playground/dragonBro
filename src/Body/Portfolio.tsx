116a0c87924ed3e71c9d3689b197ab6ae8f57349
dreamerryao <zuobinwang3@gmail.com>
2022-04-02 23:23:24 +0800

    ok

--- src/Body/Portfolio.tsx
+++ src/Body/Portfolio.tsx
@@ -1,5 +1,16 @@
 import stepnPng from "@/assets/stepn.png";
-import proplePng from "@/assets/people.png";
+import peoplePng from "@/assets/people.png";
+
+const portfolios = [
+  {
+    src: stepnPng,
+    title: "STEPN",
+  },
+  {
+    src: peoplePng,
+    title: "constitution DAO",
+  },
+];
 const Portfolio = () => {
   return (
     <div
@@ -9,36 +20,25 @@ const Portfolio = () => {
     >
       <div className="pt-16 text-4xl font-bold text-red-400">——Portfolio——</div>
       <div className="flex flex-wrap gap-10 mt-8 items-center">
-        <div className="flex flex-col justify-center items-center flex-1">
-          <div
-            style={{ height: 80 }}
-            className="w-full flex justify-center items-center"
-          >
-            <img
-              style={{ maxWidth: 200, maxHeight: 200 }}
-              src={proplePng}
-              alt="people"
-            ></img>
-          </div>
-          <div className="py-2 my-4 border-t-4 w-full text-center border-red-300 text-red-400 text-lg font-bold">
-            constitution DAO
-          </div>
-        </div>
-        <div className="flex flex-col justify-center items-center flex-1">
-          <div
-            style={{ height: 80 }}
-            className="w-full flex justify-center items-center"
-          >
-            <img
-              style={{ maxWidth: 200, maxHeight: 200 }}
-              src={stepnPng}
-              alt="stepn"
-            ></img>
-          </div>
-          <div className="py-2 my-4 border-t-4 w-full text-center border-red-300 text-red-400 text-lg font-bold">
-            STEPN
-          </div>
-        </div>
+        {portfolios.map((x) => {
+          return (
+            <div className="flex flex-col justify-center items-center flex-1">
+              <div
+                style={{ height: 80 }}
+                className="w-full flex justify-center items-center"
+              >
+                <img
+                  style={{ maxWidth: 200, maxHeight: 200 }}
+                  src={x.src}
+                  alt="people"
+                ></img>
+              </div>
+              <div className="py-2 my-4 border-t-4 w-full text-center border-red-300 text-red-400 text-lg font-bold">
+                {x.title}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/Body/index.tsx
+++ src/Body/index.tsx
@@ -4,7 +4,7 @@ import Portfolio from "./Portfolio";
 const Body = () => {
   return (
     <div className="w-full transition duration-600 ease-out ">
-      <Home message={"Hello! This is  \n DragonBro's playground."} />
+      <Home message={"Rambunctious Investment."} />
       <Portfolio />
       {/* <Contact /> */}
     </div>
